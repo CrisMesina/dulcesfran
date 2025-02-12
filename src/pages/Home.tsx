9ccebfb4b9edb919abeaf27b24e4e0e512d17820
AntiKrist0 <cristophermatiasmesina@gmail.com>
2025-02-12 02:00:39 -0300

    init proyecto

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+
+export const Home = () => {
+  return (
+    <div className="w-full h-full">
+        <div>
+            <img src="/logo.png" className="mx-auto w-96" alt="" />
+        </div>
+        <h1 className="text-5xl font-extrabold text-center w-full">Estamos trabajando en la pagina!</h1>
+    </div>
+  )
+}
